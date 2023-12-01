bc37954a17839f221e1ec1e2924e0d700af8eb02
FrdHuy <73607369+FrdHuy@users.noreply.github.com>
2023-11-30 23:39:11 -0600

    Redirect to loginpage if not logged in

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -4,43 +4,55 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue')
-  },
-  {
-    path: '/removed',
-    name: 'remove',
-    component: () => import('../views/removed.vue')
-  },
-  {
-    path: '/history',
-    name: 'history',
-    component: () => import('../views/history.vue')
-  },
-  {
-    path: '/edit',
-    name: '/edit',
-    component: () => import('../views/edit.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/Register.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('../views/HomeView.vue')
+    },
+    {
+        path: '/removed',
+        name: 'remove',
+        component: () => import('../views/removed.vue')
+    },
+    {
+        path: '/history',
+        name: 'history',
+        component: () => import('../views/history.vue')
+    },
+    {
+        path: '/edit',
+        name: '/edit',
+        component: () => import('../views/edit.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () => import('../views/Register.vue')
+    }
 ]
 
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
+
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register']; // Paths of public pages that can be accessed without logging in
+    const authRequired = !publicPages.includes(to.path); // Pages that require authentication
+    const loggedIn = localStorage.getItem('authToken'); // Check if the token exists in local storage
+
+    if (authRequired && !loggedIn) {
+        return next('/login'); // Redirect to login page if authentication is required but user is not logged in
+    }
+
+    next(); // Otherwise, continue with the normal routing
+});
 
 export default router
