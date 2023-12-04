47d547e1099a3dfd1ace5a4e85bad7f635e2544b
Aseem Sharma <aseemsharma@protonmail.com>
2023-12-03 23:21:18 -0600

    Add ucsdata crawl process

--- skynet_scrapy/myspider/skynet.py
+++ skynet_scrapy/myspider/skynet.py
@@ -1,6 +1,7 @@
 from myspider.spiders.orbitalfocusspider import OrbitalfocusspiderSpider
 from myspider.spiders.planet4589 import Planet4589spiderSpider
 from myspider.spiders.reentrypredictor import ReentrypredictorSpider
+from myspider.spiders.ucsdata import UcsdataSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from myspider.Gatherer import Gatherer
@@ -13,6 +14,7 @@ def main():
     process.crawl(OrbitalfocusspiderSpider)
     process.crawl(Planet4589spiderSpider)
     process.crawl(ReentrypredictorSpider)
+    process.crawl(UcsdataSpider)
     process.start()
     gatherer = Gatherer()
     gatherer.gather()
