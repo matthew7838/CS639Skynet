cf3a0726759dff3443f10ba23bd96409f5f707e7
Aseem Sharma <aseemsharma@protonmail.com>
2023-12-10 20:54:09 -0600

    updated thespacereportspider to parallely add launch_vehicle, launch_site and mission_sector data to planet4589 table

--- skynet_scrapy/myspider/myspider/spiders/thespacereport.py
+++ skynet_scrapy/myspider/myspider/spiders/thespacereport.py
@@ -44,6 +44,38 @@ class TheSpaceReportSpider(scrapy.Spider):
         options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(options=options, service=service)
 
+        hostname = 'localhost'
+        username = 'skynetapp'
+        password = 'skynet'
+        # database = 'skynetapp' # not necessary
+        # port = 5432 # using default
+        
+        self.connection = psycopg2.connect(host=hostname, user=username, password=password)
+        self.cur = self.connection.cursor()
+        self.add_column_if_not('planet4589', 'launch_vehicle', 'VARCHAR(255)')
+        self.add_column_if_not('planet4589', 'launch_site', 'VARCHAR(255)')
+        self.add_column_if_not('planet4589', 'mission_sector', 'VARCHAR(255)')
+
+    def add_column_if_not(self, table_name, column_name, column_type):
+        # Check if the column exists in the table
+        check_column_query = """
+            SELECT EXISTS (
+                SELECT 1
+                FROM information_schema.columns
+                WHERE table_name = %s
+                AND column_name = %s
+            );
+        """
+        # Execute the query to check if the column exists
+        self.cur.execute(check_column_query, (table_name, column_name))
+        column_exists = self.cur.fetchone()[0]
+        
+        if not column_exists:
+            # Add the column if it doesn't exist
+            add_column_query = f"ALTER TABLE {table_name} ADD COLUMN {column_name} {column_type};"
+            self.cur.execute(add_column_query)
+            self.connection.commit()
+
     def parse(self, response):
         url = response.url
         self.driver.get(url)
@@ -59,17 +91,20 @@ class TheSpaceReportSpider(scrapy.Spider):
             for tr in table_body.find_elements(By.TAG_NAME, "tr"):
                 thespacereportitem = TheSpaceReportItem()
                 tds = tr.find_elements(By.TAG_NAME, "td")
-                thespacereportitem["LaunchID"] = tds[0].text
+                launchid = thespacereportitem["LaunchID"] = tds[0].text
                 thespacereportitem["DateTime"] = tds[1].text
-                thespacereportitem["LaunchVehicle"] = tds[2].text
+                launch_vehicle = thespacereportitem["LaunchVehicle"] = tds[2].text
                 thespacereportitem["OperatorCountry"] = tds[3].text
-                thespacereportitem["LaunchSite"] = tds[4].text
+                launch_site = thespacereportitem["LaunchSite"] = tds[4].text
                 thespacereportitem["Status"] = tds[5].text
-                thespacereportitem["MissionSector"] = tds[6].text
+                mission_sector = thespacereportitem["MissionSector"] = tds[6].text
                 thespacereportitem["Crewed"] = tds[7].text
                 thespacereportitem["FirstStageRecovery"] = tds[8].text
 
                 yield thespacereportitem
+                print(f'LaunchID(Cospar): {thespacereportitem["LaunchID"]}')
+                # set data on planet4589table
+                self.set_data(launch_vehicle=launch_vehicle, launch_site=launch_site, mission_sector=mission_sector, launchid=launchid, connection=self.connection)
                 self.scraped_items.append(thespacereportitem)
 
             stop = self.driver.find_element(By.ID, "table_3_info")
@@ -80,6 +115,31 @@ class TheSpaceReportSpider(scrapy.Spider):
             self.driver.execute_script("arguments[0].click();", next)
             time.sleep(1.5)
 
+    def set_data(self, launch_vehicle, launch_site, mission_sector, launchid, connection):
+        print(f'getting launch vehicle {launch_vehicle} for cospar {launchid}')
+        #query = "UPDATE planet4589 SET launch_vehicle = %s WHERE piece LIKE %s"
+        query = "UPDATE planet4589 SET launch_vehicle = %s, launch_site = %s, mission_sector = %s WHERE piece LIKE %s"
+
+
+        with connection.cursor() as cure:
+            try:
+                #like_pattern = f"'{launchid}%'"
+                #launch_vehicle_quoted = f"'{launch_vehicle}'"
+                like_pattern = f'{launchid}%'
+                launch_vehicle_quoted = f'{launch_vehicle}'
+                launch_site_quoted = f'{launch_site}'
+                mission_sector_quoted = f'{mission_sector}'
+                print(f"Executing query: {query % (launch_vehicle_quoted, launch_site_quoted, mission_sector_quoted, like_pattern)}")
+                #input()
+                cure.execute(query, (launch_vehicle_quoted, launch_site_quoted, mission_sector_quoted, like_pattern))
+                print(f"Rows affected: {cure.rowcount}")
+                connection.commit()
+                print("Query executed successfully!")
+            except Exception as e:
+                connection.rollback()
+                print(f"Error executing query: {e}")
+                import traceback
+                traceback.print_exc()
 
     def closed(self, reason):
         self.driver.quit()
